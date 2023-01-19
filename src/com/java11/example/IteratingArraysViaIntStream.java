8e42c695d6f05e2c67ee4c8e3d81c4ebd169f52b
Abdul Hasib Yousufzai <yousufzai.abdulhasib@gmail.com>
2023-01-18 19:57:44 -0800

    added code to the iteratingarraysviaintstream file

--- src/com/java11/example/IteratingArraysViaIntStream.java
+++ src/com/java11/example/IteratingArraysViaIntStream.java
@@ -1,4 +1,13 @@
 package com.java11.example;
 
+import java.util.stream.IntStream;
+
 public class IteratingArraysViaIntStream {
+    static String [] animals ={"cat","dog","tiger"};
+
+    public static void main(String[] args) {
+        IntStream.range(0,animals.length).forEach(i->{
+            System.out.printf("%d. %s\n",i,animals[i]);
+        });
+    }
 }
